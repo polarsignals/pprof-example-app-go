a4ee160d63cd6832e4d95d52dbb31352fe8e9380
Kemal Akkoyun <kakkoyun@gmail.com>
2021-10-15 14:55:41 +0200

    Add ability to output profiles
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/pkg/profile"
+
 	"github.com/polarsignals/pprof-example-app-go/fib"
 )
 
@@ -18,21 +20,28 @@ var (
 )
 
 const (
-	modeBusyCPU = "busyCPU"
+	modeBusyCPU  = "busyCPU"
 	modeAllocMem = "allocMem"
 )
 
 func main() {
 	var (
-		bind = ""
-		mode = "both" // busyCpu, allocMem
+		bind   = ""
+		mode   = "both" // busyCpu, allocMem
+		output = false
 	)
 	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	flagset.StringVar(&bind, "bind", ":8080", "The socket to bind to.")
-	flagset.StringVar(&mode, "mode", "both", "The mode to run. Options: busyCPU, allocMem, both")
+	flagset.StringVar(&mode, "mode", "both", "The mode to run. Options: busyCPU, allocMem, both.")
+	flagset.BoolVar(&output, "output", false, "Whether to output a profile to current directory. Only works for individual modes.")
 	err := flagset.Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
 	}
 
 	log.Println("Starting HTTP server on", bind)
@@ -46,8 +55,16 @@ func main() {
 
 	switch mode {
 	case modeBusyCPU:
+		if output {
+			prof := profile.Start(profile.CPUProfile, profile.ProfilePath(path), profile.NoShutdownHook)
+			defer prof.Stop()
+		}
 		go busyCPU()
 	case modeAllocMem:
+		if output {
+			prof := profile.Start(profile.MemProfile, profile.ProfilePath(path), profile.NoShutdownHook)
+			defer prof.Stop()
+		}
 		go allocMem()
 	default:
 		go busyCPU()
@@ -58,6 +75,10 @@ func main() {
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	defer signal.Stop(c)
 	<-c
+
+	if mode != "both" && output {
+		log.Println("profile: caught interrupt, stopping profiles")
+	}
 }
 
 // Calculates Fibonacci numbers starting with 1 000 000th.
